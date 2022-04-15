3068023c31b0d07c2c8fef15959d3db2ed6c4c6c
did3706@naver.com <did3706@naver.com>
2022-04-15 16:50:10 +0900

    Binary

---
+++ Chapter1/src/ch04/BinartyTest.java
@@ -0,0 +1,21 @@
+package ch04;
+
+public class BinartyTest {
+
+	public static void main(String[] args) {
+
+		
+		// 식별자 표시
+		int num = 10;
+		int bNum = 0B1010; //0B뒤에 나오는 숫자는 10을 의미하며, 2진수다
+		int oNum = 012; // 0을 사용하면 8진수를 의미
+		int xNum = 0XA; //16진수 앞에는 0X;
+		
+		System.out.println(num);
+		System.out.println(bNum);
+		System.out.println(oNum);
+		System.out.println(oNum);
+		
+	}
+
+}
