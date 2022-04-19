393603773df76b2ea1dad4c20d754de68c18a551
did3706@naver.com <did3706@naver.com>
2022-04-20 00:35:53 +0900

    [형변환]  실수 - double

---
+++ Chapter1/src/ch07/DoubleTest.java
@@ -0,0 +1,16 @@
+package ch07;
+
+public class DoubleTest {
+
+	public static void main(String[] args) {
+
+		 // double 이 더 편하므로, 큰 문제 없으면 double 사용해도 됨
+		double dnum = 3.14;
+		float fnum = 3.14f;
+		
+		System.out.println(dnum);
+		System.out.println(fnum);
+
+	}
+
+}
